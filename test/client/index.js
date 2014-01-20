266d8711755dd977a2e0978ad7b0cf1a550415fd
LastLeaf <bqfu@163.com>
2014-01-20 23:29:17 +0800

    HTTP routes implemented.

--- test/client/index.js
+++ test/client/index.js
@@ -1,7 +1,4 @@
 // Copyright 2014 LastLeaf, LICENSE: github.lastleaf.me/MIT
-//$title	fw.mpa test!
-//$require	index.css index.tmpl
-//$reload	none
 'use strict';
 
-fw.wrapper.innerHTML = fw.tmpl('index', 'Hello world!');
+document.body.innerHTML = tmpl.index('Hello world!');
